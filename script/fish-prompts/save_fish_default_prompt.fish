a9ee73d8d416095ff050ec262053bbac489df23f
qianlitt <zvdiek@gmail.com>
2025-03-19 01:34:46 +0800

    feat(script): Check the files in subdiretory before copy

--- script/fish-prompts/save_fish_default_prompt.fish
+++ script/fish-prompts/save_fish_default_prompt.fish
@@ -56,6 +56,21 @@ function save_prompt_list \
     fish_config prompt list >$argv[1]
 end
 
+function check_subdir \
+    --description "if sub-diretory exist prompt files, return 1; otherwise return 0"
+
+    set target_file
+    for path in $argv[2]
+        set -a target_file (string split -r -m1 / $path)[2]
+    end
+
+    is_file (string join '/' -- $argv[1] $target_file)
+    if test $status -eq 1
+        return 1
+    end
+    return 0
+end
+
 # Save fish prompts
 function save_prompt
     set -f prompt_subdir
@@ -69,12 +84,24 @@ function save_prompt
             mkdir -p $prompt_subdir
         end
 
-        # fish save prompt
-        yes y | fish_config prompt save $prompt
-
-        # Copy fish prompt to sub-diretory
+        # Check sub-diretory's file
+        set copy_flag 0
         for file in $prompt_path
-            cp -v $file $prompt_subdir
+            check_subdir $prompt_subdir $file
+            if test $status -eq 0 # if file isn't exist
+                set copy_flag 1
+            end
+        end
+
+        # fish save prompt
+        if test $copy_flag -eq 1
+            printf "%s%s%s:\n" \
+                (set_color green) $prompt (set_color normal)
+            yes y | fish_config prompt save $prompt
+            # Copy fish prompt to sub-diretory
+            for file in $prompt_path
+                cp -v $file $prompt_subdir
+            end
         end
     end
 end
@@ -82,6 +109,8 @@ end
 function main
     save_prompt_list $prompt_list_path
     save_prompt $prompt_list_path
+    printf "%sCompleted!%s" \
+        (set_color green) (set_color normal)
 end
 
 main
