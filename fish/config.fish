1879ff56038ade3643da5fceae2502de67711c1a
qianlitt <zvdiek@gmail.com>
2025-03-18 20:59:43 +0800

    feat(fish/conf): Set prompts dynamically by fish's context

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set env_type (prompt_check_env)
+switch $env_type
+    case ssh
+        prompt_set_color
+    case kitty
+        starship init fish | source
+        prompt_set_color
+    case foot
+        starship init fish | source
+        prompt_set_color
+end
