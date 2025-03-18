460b338a1aec2f67a6a0a80a5cbdcf0aaf582ada
qianlitt <zvdiek@gmail.com>
2025-03-18 23:41:00 +0800

    refactor(script): Refactor `save_fish_default_prompt.fish`
    - Use `is_file` and `is_dir` instead of `is_path_exist`

--- script/fish-prompts/save_fish_default_prompt.fish
+++ script/fish-prompts/save_fish_default_prompt.fish
@@ -14,30 +14,28 @@ set prompt_left_path (string join '/' -- $prompt_dir $prompt_left_file)
 set prompt_right_file "fish_right_prompt.fish"
 set prompt_right_path (string join '/' -- $prompt_dir $prompt_right_file)
 
-function is_path_exist
+function is_file \
+    --description "if argv[1] is a file, then return 1, otherwise return 0."
     if test -f $argv[1]
-        printf "%sYes, %s is a file.%s\n" \
-            (set_color green) $argv[1] (set_color normal)
         return 1
-    else if test -d $argv[1]
-        printf "%sYes, %s is a directory.%s\n" \
-            (set_color green) $argv[1] (set_color normal)
-        return 2
-    else
-        printf "%sNo, %s is not exist.%s\n" \
-            (set_color $fish_color_error) $argv[1] (set_color normal)
-        return -1
     end
+    return 0
 end
 
-# List all default prompts for fish
-function save_prompt_list
-    begin
-        set -f output (is_path_exist $argv[1])
-        set -f retvar $status
-    end
+function is_dir \
+    --description "if argv[1] is a directory, then return 1, otherwise return 0."
 
-    if test $retvar -eq 1
+    if test -d $argv[1]
+        return 1
+    end
+    return 0
+end
+
+function check_prompt_list_file \
+    --description "if argv[1] isn't exist, then create."
+
+    is_file $argv[1]
+    if test $status -eq 1
         printf "%sFile %s\"%s\"%s is exist.%s\n" \
             (set_color green) (set_color grey) $argv[1] (set_color green) (set_color normal)
     else
@@ -45,7 +43,12 @@ function save_prompt_list
             (set_color $fish_color_error) (set_color grey) $argv[1] (set_color $fish_color_error) (set_color normal)
         touch $argv[1]
     end
+end
 
+function save_prompt_list \
+    --description "check argv[1](file), and write prompt list in it."
+
+    check_prompt_list_file $argv[1]
     fish_config prompt list >$argv[1]
 end
 
@@ -55,7 +58,8 @@ function save_prompt
     for prompt in (awk '{print $1}' $argv[1])
         # Create prompt sub-diretory
         set prompt_subdir (string join '/' -- $script_dir $prompt)
-        if not test -d $prompt_subdir
+        is_dir $prompt_subdir
+        if test $status -eq 0 # if $prompt_subdir isn't exist
             printf "%smkdir:%s %s\n" \
                 (set_color green) (set_color normal) $prompt_subdir
             mkdir -p $prompt_subdir
@@ -65,19 +69,17 @@ function save_prompt
         yes y | fish_config prompt save $prompt
 
         # Copy fish prompt to sub-diretory
-        is_path_exist $argv[2]
-        set -f retvar $status
-        if test $retvar -eq 1
-            cp $argv[2] $prompt_subdir
+        is_file $argv[2] # check fish_prompt.fish
+        if test $status -eq 1
+            cp -v $argv[2] $prompt_subdir
         else
             printf "%sError: %s\"%s\"%s isn't exist.%s\n" \
                 (set_color $fish_color_error) (set_color grey) argv[2] (set_color $fish_color_error) (set_color normal)
         end
 
-        is_path_exist $argv[3]
-        set -f retvar $status
-        if test $retvar -eq 1
-            cp $argv[3] $prompt_subdir
+        is_file $argv[3] # check fish_right_prompt.fish
+        if test $status -eq 1
+            cp -v $argv[3] $prompt_subdir
         else
             printf "%sError: %s\"%s\"%s isn't exist.%s\n" \
                 (set_color $fish_color_error) (set_color grey) argv[3] (set_color $fish_color_error) (set_color normal)
