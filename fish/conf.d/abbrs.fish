57f47ff061707d0fd97f9dae59cd80ac769cfefa
qianlitt <zvdiek@gmail.com>
2025-07-08 17:12:12 +0800

    feat(fish/conf): add universal extractor function

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -11,6 +11,40 @@ else
     abbr -a -- la "ll -A"
 end
 
+# extract
+function ex -a file
+    if test -f "$file"
+        switch "$file"
+            case "*.tar.bz2"
+                tar xvjf $file
+            case "*.tar.gz"
+                tar xvzf $file
+            case "*.bz2"
+                bunzip2 $file
+            case "*.rar"
+                unrar x $file
+            case "*.gz"
+                gunzip $file
+            case "*.tar"
+                tar xvf $file
+            case "*.tbz2"
+                tar xvjf $file
+            case "*.tgz"
+                tar xvzf $file
+            case "*.zip"
+                unzip $file
+            case "*.Z"
+                uncompress $file
+            case "*.7z"
+                7z x $file
+            case "*"
+                set_color red; echo "'$file' cannot be extracted via ex()"
+        end
+    else
+        set_color red; echo "'$file' is not a valid file"
+    end
+end
+
 abbr -a -- ip "ip -c"
 abbr -a -- df "df -h"
 
